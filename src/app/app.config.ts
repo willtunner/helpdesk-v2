a945edd5afd2cb15cc90aa445eb3a661775fad37
william <williampereira21@gmail.com>
2025-06-27 15:31:32 -0300

    firebase e rich text ok

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,38 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideNgxMask } from 'ngx-mask';
-
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from '../app/environments/environment'
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { provideQuillConfig } from 'ngx-quill';
+import { provideNgxMask } from 'ngx-mask';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
     provideNgxMask(),
-    provideRouter(routes), provideAnimationsAsync()]
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: environment.firebase.apiKey,
+        authDomain: environment.firebase.authDomain,
+        projectId: environment.firebase.projectId,
+        storageBucket: environment.firebase.storageBucket,
+        messagingSenderId: environment.firebase.messagingSenderId,
+        appId: environment.firebase.appId,
+        measurementId: environment.firebase.measurementId,
+      })
+    ),
+    provideStorage(() => getStorage()),
+    provideFirestore(() => getFirestore()),
+    provideQuillConfig({
+      modules: {
+        table: true, // Habilita o módulo de tabelas
+        toolbar: [
+          // Sua configuração de toolbar pode ser replicada aqui se necessário
+        ]
+      },
+      theme: 'snow'
+    })
+  ],
 };
---
+++ src/app/shared/components/rich-text/rich-text.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rich-text',
+  standalone: true,
+  imports: [],
+  templateUrl: './rich-text.component.html',
+  styleUrl: './rich-text.component.scss'
+})
+export class RichTextComponent {
+
+}
