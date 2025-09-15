5aced8667fe735b806ecde7687ea1c5fcc4cef60
william <williampereira21@gmail.com>
2025-09-15 16:18:19 -0300

    fix moderate rich text

--- src/app/shared/components/rich-text/rich-text.component.ts
+++ src/app/shared/components/rich-text/rich-text.component.ts
@@ -1,13 +1,37 @@
-// rich-text.component.ts (VERSÃO CORRIGIDA)
-import { Component, forwardRef, Input } from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR, FormsModule } from '@angular/forms';
-import { QuillModule } from 'ngx-quill';
 import { CommonModule } from '@angular/common';
+import { 
+  Component, 
+  forwardRef, 
+  Input, 
+  ViewChild, 
+  ElementRef,
+  OnInit, 
+  AfterViewInit,
+  DoCheck,
+  Optional,
+  Self,
+  Injector
+} from '@angular/core';
+import { 
+  FormsModule, 
+  NG_VALUE_ACCESSOR, 
+  ControlValueAccessor,
+  NgControl,
+  FormControl
+} from '@angular/forms';
+import { QuillModule } from 'ngx-quill';
+import { MatFormFieldControl } from '@angular/material/form-field';
+import { Subject } from 'rxjs';
+import { coerceBooleanProperty } from '@angular/cdk/coercion';
 
 @Component({
   selector: 'app-rich-text-editor',
   standalone: true,
-  imports: [CommonModule, FormsModule, QuillModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    QuillModule
+  ],
   templateUrl: './rich-text.component.html',
   styleUrls: ['./rich-text.component.scss'],
   providers: [
@@ -15,49 +39,149 @@ import { CommonModule } from '@angular/common';
       provide: NG_VALUE_ACCESSOR,
       useExisting: forwardRef(() => RichTextEditorComponent),
       multi: true
+    },
+    {
+      provide: MatFormFieldControl,
+      useExisting: RichTextEditorComponent
     }
   ]
 })
-export class RichTextEditorComponent implements ControlValueAccessor {
+export class RichTextEditorComponent implements ControlValueAccessor, MatFormFieldControl<string>, OnInit, AfterViewInit, DoCheck {
+  @ViewChild('editor') editor!: ElementRef;
   @Input() placeholder: string = 'Digite seu texto aqui...';
-  @Input() height: string = '300px';
+  @Input() height: string = '150px';
+
+  static nextId = 0;
+  id = `rich-text-${RichTextEditorComponent.nextId++}`;
+  stateChanges = new Subject<void>();
+  focused = false;
+  errorState = false;
+  controlType = 'rich-text';
+  describedBy = '';
   
-  value: string = '';
-  isDisabled = false;
+  private _value: string = '';
+  private _required = false;
+  private _disabled = false;
+  ngControl: NgControl | null = null;
 
   quillModules = {
     toolbar: [
       ['bold', 'italic', 'underline', 'strike'],
       [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
       [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+      [{ 'size': ['small', false, 'large', 'huge'] }],
       [{ 'color': [] }, { 'background': [] }],
+      [{ 'font': [] }],
+      [{ 'align': [] }],
       ['link', 'image', 'video'],
-      ['clean']
+      [{ 'table': 'insert-table' }]
     ]
   };
 
-  onChange: (value: string) => void = () => {};
-  onTouched: () => void = () => {};
+  onChange = (_: any) => {};
+  onTouched = () => {};
 
-  writeValue(value: string): void {
-    this.value = value || '';
+  constructor(private injector: Injector) {}
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.ngControl = this.injector.get(NgControl, null);
+      if (this.ngControl) {
+        this.ngControl.valueAccessor = this;
+      }
+    });
   }
 
-  registerOnChange(fn: (value: string) => void): void {
+  ngAfterViewInit(): void {
+    setTimeout(() => this.stateChanges.next());
+  }
+
+  ngDoCheck(): void {
+    if (this.ngControl) {
+      this.errorState = (this.ngControl.invalid ?? false) && (this.ngControl.touched ?? false);
+      this.stateChanges.next();
+    }
+  }
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
     this.onChange = fn;
   }
 
-  registerOnTouched(fn: () => void): void {
+  registerOnTouched(fn: any): void {
     this.onTouched = fn;
   }
 
   setDisabledState(isDisabled: boolean): void {
-    this.isDisabled = isDisabled;
+    this.disabled = isDisabled;
+  }
+
+  get value(): string {
+    return this._value;
+  }
+
+  set value(value: string) {
+    this._value = value;
+    this.onChange(value);
+    this.stateChanges.next();
+  }
+
+  get empty(): boolean {
+    return !this._value;
+  }
+
+  get shouldLabelFloat(): boolean {
+    return this.focused || !this.empty;
+  }
+
+  @Input()
+  get required(): boolean {
+    return this._required;
+  }
+
+  set required(req: boolean) {
+    this._required = coerceBooleanProperty(req);
+    this.stateChanges.next();
+  }
+
+  @Input()
+  get disabled(): boolean {
+    return this._disabled;
+  }
+
+  set disabled(dis: boolean) {
+    this._disabled = coerceBooleanProperty(dis);
+    this.stateChanges.next();
+  }
+
+  onContainerClick(event: MouseEvent): void {
+    if (this.editor?.nativeElement) {
+      this.editor.nativeElement.focus();
+    }
+  }
+
+  setDescribedByIds(ids: string[]): void {
+    this.describedBy = ids.join(' ');
   }
 
   onContentChange(content: string): void {
     this.value = content;
-    this.onChange(content);
     this.onTouched();
+  }
+
+  onFocus(): void {
+    if (!this.focused) {
+      this.focused = true;
+      this.stateChanges.next();
+    }
+  }
+
+  onBlur(): void {
+    this.focused = false;
+    this.onTouched();
+    this.stateChanges.next();
   }
 }
